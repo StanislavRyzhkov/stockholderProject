6da46ba3d1686a89ed817b0120cac87143627f6d
StanislavRyzhkov <30403280+StanislavRyzhkov@users.noreply.github.com>
2019-12-24 21:18:35 +0700

    Update TokenProvider.kt

--- src/main/kotlin/company/ryzhkov/sh/security/TokenProvider.kt
+++ src/main/kotlin/company/ryzhkov/sh/security/TokenProvider.kt
@@ -13,6 +13,7 @@ import org.springframework.security.core.userdetails.ReactiveUserDetailsService
 import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.stereotype.Component
 import reactor.core.publisher.Mono
+import reactor.core.scheduler.Schedulers
 import java.security.Key
 import java.util.*
 import javax.annotation.PostConstruct
@@ -60,24 +61,23 @@ import javax.crypto.spec.SecretKeySpec
             .compact()
     }
 
-    fun getUsername(request: ServerHttpRequest): Mono<String> = Mono.fromCallable {
-        val list = request.headers["Authorization"]
-
-        if (list == null || list.isEmpty()) {
-            throw AuthException(ACCESS_DENIED)
+    fun getUsername(request: ServerHttpRequest): Mono<String> = Mono
+        .fromCallable {
+            val list = request.headers["Authorization"]
+            if (list == null || list.isEmpty()) {
+                throw AuthException(ACCESS_DENIED)
+            }
+            val authHeader = list[0]
+            if (authHeader == null || !authHeader.startsWith("Bearer")) {
+                throw AuthException(ACCESS_DENIED)
+            }
+            val token = authHeader.substring("Bearer".length).trim()
+            Jwts.parser()
+                .setSigningKey(key)
+                .parseClaimsJws(token)
+                .body
+                .subject
         }
-
-        val authHeader = list[0]
-
-        if (authHeader == null || !authHeader.startsWith("Bearer")) {
-            throw AuthException(ACCESS_DENIED)
-        }
-
-        val token = authHeader.substring("Bearer".length).trim()
-        Jwts.parser()
-            .setSigningKey(key)
-            .parseClaimsJws(token)
-            .body
-            .subject
-    }.onErrorMap { AuthException(ACCESS_DENIED) }
+        .onErrorMap { AuthException(ACCESS_DENIED) }
+        .subscribeOn(Schedulers.elastic())
 }
